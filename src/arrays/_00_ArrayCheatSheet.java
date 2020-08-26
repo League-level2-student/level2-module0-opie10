6b16df85bb927941a92ba0750b375f703fff1aa1
augie <augie@keatinge.com>
2020-08-25 18:33:23 -0700

    fun :smile:
    good. orking on race of robots. arrays interesting.

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -19,17 +19,39 @@ public class _00_ArrayCheatSheet {
 		}
 		
 		//6. make an array of 50 integers
-		int[]ints = new int[50];
+		int[]ints = new int[250];
+		int yay = 0;
 		//7. use a for loop to make every value of the integer array a random number
-		for (int i = 0; i < ints.length; i++) {
+		for (int i = 0; i < 100000; i++) {
+			
+		 yay = yay+1;
+		for (int l = 0; l < ints.length; l++) {
 			Random rd = new Random();
-			ints[i] = rd.nextInt(50) ;
-			System.out.println(ints[i]);
+			ints[l] = rd.nextInt(250) ;
+			System.out.println(ints[l]);
 		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		int currentsn = 250 ;
+		for (int h = 0; h < ints.length; h++) {
+			if (ints[h] < currentsn) {
+				currentsn = ints[h] ;
+			}
+		}
+		System.out.println(currentsn +" is the smallest number.");
 		//9 print the entire array to see if step 8 was correct
 		
 		//10. print the largest number in the array.
+		int currentln = -1 ;
+		for (int o = 0; o < ints.length; o++) {
+			if (ints[o] > currentln) {
+				currentln = ints[o] ;
+			}
+		}
+		System.out.println(currentln + " is the largest number.");
+		if (currentsn >9) {
+			System.out.println("It took " + yay + " times to get above 10 as the smallest number.");
+			System.exit(0);
+		}
+		}
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,78 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
-	//1. make a main method
+	// 1. make a main method
+	public static void main(String[] args) {
 
-	//2. create an array of 5 robots.
+		// 2. create an array of 5 robots.
+		Robot[] garhy = new Robot[77];
+	
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < garhy.length; i++) {
+			garhy[i] = new Robot();
+			garhy[i].setX(15 + 25 * i);
+			garhy[i].setY(950 );
+			garhy[i].miniaturize();
+			garhy[i].setWindowSize(1920, 1080);
+			garhy[i].penDown();
+			garhy[i].setRandomPenColor();
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-	//3. use a for loop to initialize the robots.
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+		boolean rip = true;
+int smallesty = 1000 ;
+int fr = -1 ;		
+		
+		
+		
+		
+		
+		
+		
+		while (rip) {
+			for (int i = 0; i < garhy.length; i++) {
+				int y = garhy[i].getY();
+				garhy[i].setSpeed(10);
+				if (y <= 30) {
+					rip = false;
+					
+					JOptionPane.showMessageDialog(null, "ROBOT " + i +" WON!!  THROW A PARTY!! or if u weren't supporting them be salty... but that your choice. PARTY TIMEEEE");
+					
+					break;
+					
+					
+				}
+				if (y<smallesty) {
+					smallesty = y ;
+					if (fr != -1) {
+						garhy[fr].miniaturize();
+					}
+					
+					fr = i ;
+				}
+				Random hoi = new Random();
+				int hi = hoi.nextInt(51);
+				garhy[i].move(hi);
+			}
+			// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+			// screen.
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
+			// 7. declare that robot the winner and throw it a party!
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+			// 8. try different races with different amounts of robots.
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
-	//7. declare that robot the winner and throw it a party!
-
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
+			// 9. make the robots race around a circular track.
+			if (fr != -1) {
+				garhy[fr].
+			}
+		}
+	}
 }
